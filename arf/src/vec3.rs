f917c27286d7172de8f8610c2b1e62c16554651c
zisarikaya <ziya.sarikayaziya@zalando.de>
2021-12-16 11:39:27 +0100

    impl calculating two vectors cross product

--- arf/src/vec3.rs
+++ arf/src/vec3.rs
@@ -48,6 +48,11 @@ impl Vector3 {
     pub fn multiply(v1: Vector3, v2: Vector3) -> Self {
         return Self { x: v1.x * v2.x, y: v1.y * v2.y, z: v1.z * v2.z };
     }
+
+    /// Calculate two vectors cross product
+    pub fn cross_product(v1: Vector3, v2: Vector3) -> Vector3 {
+        return Self { x: v1.y * v2.z - v1.z * v2.y, y: v1.z * v2.x - v1.x * v2.z, z: v1.x * v2.y - v1.y * v2.x };
+    }
 }
 
 /// The addition operator +
